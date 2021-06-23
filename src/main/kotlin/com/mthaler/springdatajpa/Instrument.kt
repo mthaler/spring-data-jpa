edaa9f35cd50cecd6c7d8535f2373f0c23f9ec1c
Michael Thaler <michael.thaler@dlr.de>
2021-06-23 08:52:01 +0200

    Added entity classes.

---
+++ src/main/kotlin/com/mthaler/springdatajpa/Instrument.kt
@@ -0,0 +1,21 @@
+package com.mthaler.springdatajpa
+
+import java.io.Serializable
+import javax.persistence.*
+
+@Entity
+@Table(name = "instrument")
+class Instrument : Serializable {
+
+    @Id
+    @Column(name = "INSTRUMENT_ID")
+    var instrumentId: String? = null
+
+    @ManyToMany
+    @JoinTable(
+        name = "singer_instrument",
+        joinColumns = [JoinColumn(name = "INSTRUMENT_ID")],
+        inverseJoinColumns = [JoinColumn(name = "SINGER_ID")]
+    )
+    var singers: Set<Singer> = HashSet()
+}
