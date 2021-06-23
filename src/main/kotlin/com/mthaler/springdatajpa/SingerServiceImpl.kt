49df691e42925b3b3a2eb8374575d312e9629694
Michael Thaler <michael.thaler@dlr.de>
2021-06-23 09:07:12 +0200

    Added SingerServiceImpl.

---
+++ src/main/kotlin/com/mthaler/springdatajpa/SingerServiceImpl.kt
@@ -0,0 +1,29 @@
+package com.mthaler.springdatajpa
+
+import com.mthaler.springdatajpa.entities.Singer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service("springJpaSingerService")
+@Transactional
+class SingerServiceImpl: SingerService {
+
+    @Autowired
+    private lateinit var singerRepository: SingerRepository
+
+    @Transactional(readOnly = true)
+    override fun findAll(): List<Singer> {
+        return singerRepository.findAll().toList()
+    }
+
+    @Transactional(readOnly = true)
+    override fun findByFirstName(firstName: String): List<Singer> {
+        return singerRepository.findByFirstName(firstName)
+    }
+
+    @Transactional(readOnly = true)
+    override fun findByFirstNameAndLastName(firstName: String, lastName: String): List<Singer> {
+        return singerRepository.findByFirstNameAndLastName(firstName, lastName)
+    }
+}
